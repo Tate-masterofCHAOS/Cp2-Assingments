f38dc44ef9052b5e77501b861b3df726e105a028
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-01-08 13:46:08 -0700

    Added some basic function outlines

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -1,5 +1,31 @@
 # Tate Morgan Financial calculator 
 # This is my financial calculator
 
+# My job is to build a prpgram that completes the following functions
+# How long it will take to save for a goal based on a weekly or monthly deposit
+# Compound Interest Calculator 
+# Budget Allocator (use set percentages to divide an income into spending categories like savings, entertainment, and food)
+# Sale Price Calculator (apply discounts to prices)
+# Tip Calculator
+
+# The Steps ill follow are as follows
+# Create a main function that will run the user interface
+# Using project decomposition to figure out what other functions you need and how they interact with each other
+# Create your functions
+# Have at least 2 people test your code to make sure it works
+ 
+def CompoundInt_cal():
+    initial_amount = input("How much money do you have initially")
+    interest_rate = input("What is the interest rate")
+    pass
+
+def Budget_Allocator():
+    pass
+
+def price_cal():
+    pass
+def tip_cal():
+    pass
+
 def main():
     pass
