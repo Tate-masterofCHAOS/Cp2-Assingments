266b6bde29d115a98808e7fae1c2b5626324057a
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-01-14 13:39:48 -0700

    started library

---
+++ Book Library/main.py
@@ -0,0 +1,12 @@
+#Stores all items in a list
+#Function to add a new item
+#Function to search the items
+#Function to remove an item
+#Function that runs the code (displays the menu options inside and calls the functions inside of a while True loop)
+#Project must include
+#easy to understand variable and function names
+#Pseudocode comments explaining what the code is doing
+#Good use of white space to keep items separate and easy to read/find
+#Have at least 2 people test your code before submission!
+Books = ["Officical Legend of Zelda Historia", "Mythology", "The School for Good and Evil"]
+Author = ["Nintendo", "Mythology", "Soman Chainani"]
