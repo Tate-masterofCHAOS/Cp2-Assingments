2a3f994f0ae364f095936b0339eed238a997072e
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-01-16 13:29:25 -0700

    worked on main

--- Book Library/main.py
+++ Book Library/main.py
@@ -8,5 +8,22 @@
 #Pseudocode comments explaining what the code is doing
 #Good use of white space to keep items separate and easy to read/find
 #Have at least 2 people test your code before submission!
-Books = ["Officical Legend of Zelda Historia", "Mythology", "The School for Good and Evil"]
-Author = ["Nintendo", "Mythology", "Soman Chainani"]
+books = ["officical legend of zelda historia", "mythology", "the school for good and evil"]
+author = ["nintendo", "edith hamilton", "soman chainani"]
+def main():
+    
+    job = input("Would you like to add, remove, or search for a book: ")
+    job.lower
+    if job == "add":
+        books.append(input("What book do you wish to add: "))
+        author.append(input("What is the author of the book you wish to add: "))
+    elif job == "remove":
+        book_remove = input("What book do you wish to remove: ")
+        author_remove = input("What is the author of book you wish to remove: ")
+        books.remove(book_remove)
+        author.remove(author_remove)
+    else:
+        print("I am sorry that is not a function please double check your spelling")
+    return books, author
+
+main()
