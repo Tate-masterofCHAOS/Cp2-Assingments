9375c66df03462fe44e0d2616b19676218d0fb2b
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-01-29 12:49:50 -0700

    Update main.py

--- Password_gen/main.py
+++ Password_gen/main.py
@@ -1,33 +1,39 @@
 import string
 import random
  
+
+
+    
+ 
+
+
 def main():
     length = int(input("Enter password length: "))
     print('''Choose character set for password from these : 
          1. Digits
          2. Letters
          3. Special characters
-         4. Generate Password''')
+         4. Generate  Password''')
  
-    CharaList = ""
+    characterList = ""
  
     # Getting character set for password
     while(True):
-        requirement = int(input("Pick a number "))
-        if(requirement == 1):
+        choice = int(input("Pick a number "))
+        if(choice == 1):
          
             # Adding letters to possible characters
             characterList += string.ascii_letters
-        elif(requirement == 2):
+        elif(choice == 2):
             
             # Adding digits to possible characters
             characterList += string.digits
-        elif(requirement == 3):
+        elif(choice == 3):
          
             # Adding special characters to possible
             # characters
             characterList += string.punctuation
-        elif(requirement == 4):
+        elif(choice == 4):
             break
         else:
             print("Please pick a valid option!")
