521c88bbf403becd0563825fb238b16fd81d9df0
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-02-04 20:26:15 +0000

    didiid

--- Book Library/main.py
+++ Book Library/main.py
@@ -9,27 +9,37 @@
 #Good use of white space to keep items separate and easy to read/find
 #Have at least 2 people test your code before submission!
 
+
+
+def add(books, authors):
+    book = input("What book do you wish to add: ").lower()
+    author = input("What is the author of the book you wish to add: ").lower()
+    books.append(book)
+    authors.append(author)
+    return books, author
+
+def remove(books, authors):
+    book = input("What book do you wish to remove: ").lower()
+    author = input("What is the author of book you wish to remove: ").lower()
+    books.remove(book)
+    authors.remove(author)
+
+
+
 def main():
-    books = ()
-    authors = ()
+    books = []
+    authors = []
     while True:
-        job = input("Would you like to add, remove, or search for a book: ")
-        job.lower
+        job = input("Would you like to add, remove, or search for a book: ").lower()
         if job == "add":
-            book = input("What book do you wish to add: ")
-            author = input("What is the author of the book you wish to add: ")
-            books.append(book)
-            authors.append(author)
+            add(books, authors)
         elif job == "remove":
-            book = input("What book do you wish to remove: ")
-            author = input("What is the author of book you wish to remove: ")
-            books.remove(book)
-            authors.remove(author)
+            remove(books,authors)
         elif job == "search" or "search for a book":
             path = input("would you like to search by book or author").lower
             if path == "book":
-                book = input("What is the title of the book")
-                print books{book}
+                book = input("What is the title of the book").lower()
+                print(books)
                 pass
         else:
             print("I am sorry that is not a function please double check your spelling")
