340d958c1a2b22a8049d078732a84c754003b644
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-02-05 13:19:37 -0700

    Update main.py

--- Book Library/main.py
+++ Book Library/main.py
@@ -9,33 +9,32 @@
 #Good use of white space to keep items separate and easy to read/find
 #Have at least 2 people test your code before submission!
 
-
-
 def add(libarby):
-    book = input("What book do you wish to add: ").lower()
-    author = input("What is the author of the book you wish to add: ").lower()
-    new = book + author
-    libarby.append(new)
-    
+    book = input("What book do you wish to add: ")
+    author = input("What is the author of the book you wish to add: ")
+    libarby.add(book)
+    libarby.add(author)
+
 
 def remove(libarby):
-    book = input("What book do you wish to remove: ").lower()
-    author = input("What is the author of book you wish to remove: ").lower()
-    libarby.remove(book)
-    libarby.remove(author)
+    book = input("What book do you wish to remove: ")
+    author = input("What is the author of book you wish to remove: ")
+    libarby.remove(book, "by", author)
 
-def search(libarby):
-    book = input("What is the title of the book: ").lower()
-    for i in libarby:
-        if i == book:
-            print(libarby[i])
-
+def search(libarby,):
+        book = input("What is the title of the book: ").lower()
+        for i in libarby:
+            if i == book:
+                print(libarby)
+            else:
+                print("Sorry that is not in our library")
 
 
 def main():
-    libarby = []
+    libarby = set({""})
     while True:
-        job = input("Would you like to add, remove, or search for a book: ").lower()
+        job = input("Would you like to add, remove, or search for a book: ")
+        job.lower
         if job == "add":
             add(libarby)
         elif job == "remove":
