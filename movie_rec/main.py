55ac97c4fe5c440adfa4323e102a67b1cfc9c57b
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-02-13 13:24:51 -0700

    wokred

--- movie_rec/main.py
+++ movie_rec/main.py
@@ -3,7 +3,18 @@ import csv
 
 
 
+def open(length, director, genre):
+    file = open("movie_rec\Movies list.csv","r").read()
 
+    movies = {}
+
+    with open("movie_rec\Movies list.csv") as file:
+        reader = csv.reader(file)
+        next(reader)
+        for i in reader:
+            if i[4] >= length:
+                movies.update({i[0]:i[1]})
+                print(i)
 
 
 
@@ -11,10 +22,18 @@ def main():
     require = input("Would you like to add search requirements or print all movies press 1 for search requirments and 2 for printing all: ")
     if require == "1":
         while True:
-            requirements = input("Press number according to what you want \n1: Length \n2: Director \n3: Genre \n4:")
-            if requirements == "1":
-                pass
-
+            requirements = input("Press number according to what you want \n1: Length \n2: Director \n3: Genre \n4: move on \n")
+            if requirements == 1:
+                length = input(int("What is the length you want"))
+            if requirements == 2:
+                director = input("Who is the director you looking for")
+            if requirements == 3:
+                genre = input("What is the genre you looking for")
+            if requirements == 4:
+                open(length, director, genre)
+        
+        
+            
         
     if require == "2":
         file = open("movie_rec\Movies list.csv","r").read()
