a4e4b21ca83311b48279e038acbe5e02aa5c3a30
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-03-03 13:42:46 -0700

    eeeeeee

---
+++ multi_page/calc.py
@@ -0,0 +1,18 @@
+#Tate Morgan, Basics calculations functions
+
+num = 10
+
+def get_num():
+    return int(input("Tell me a number: "))
+
+def addition():
+    return get_num()+get_num()
+
+def subtraction(x,y):
+    return x-y
+
+def multiplication(x,y):
+    return x*y
+
+def division(x,y):
+    return x/y
