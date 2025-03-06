d95fb06265a9bfe1b028836a7d44ff9a9f952f85
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-03-06 05:37:48 +0000

    eee

--- Word_Counter/main.py
+++ Word_Counter/main.py
@@ -1,21 +1,31 @@
-
-
+import datetime
 
 def count():    
     word_count = 0
-    with open("Word_Counter/word_doc.txt", "w", newline="") as file:
-            text = input("What would you like to check the word count of").lower()
-            file.write(text)
-        
-    
-    with open("Word_Counter/word_doc.txt", "r") as file:
-        file.read(n)
-        for n in file:
-            if n == " ":
+    with open(r'Word_Counter/word_doc.txt','r') as file:
+        data = file.read()
+        lines = data.split()
+        for word in lines:
+            if not word.isnumeric():          
                 word_count += 1
-            else:
-                continue
-        word_count +=1
+        print(f"Your document is {word_count} long")
 
-count()
-        
+def main():
+    while True
+        task = input("Press 1 to edit file, 2 to check word count, or 3 to check timestamps\n")
+        if task == "1":
+            with open(r'Word_Counter/word_doc.txt','a') as file:
+                file.write(input("What would you like to add: \n"))
+                current_time = datetime.datetime.now()
+                print(current_time)
+            with open(r'Word_Counter/Timestamps.txt','a') as file:
+                timestamp = str(current_time)
+                file.write(timestamp)
+                file.write("\n")
+        if task == "2":
+            count()
+        if task == "3":
+            with open(r'Word_Counter/Timestamps.txt','r') as file:
+                file.read()
+
+main()
