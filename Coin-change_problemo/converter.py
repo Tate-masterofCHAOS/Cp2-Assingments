6036ea194fa8a54df99f0de90ae1743c19c4d079
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-03-27 13:44:14 -0600

    eeeeee

---
+++ Coin-change_problemo/converter.py
@@ -0,0 +1,55 @@
+import csv
+
+coins = []
+
+
+def calculate(target, coin_amount1, coin_amount2, coin_amount3, coin_amount4):
+    coin1 = 0
+    coin2 = 0
+    coin3 = 0
+    coin4 = 0
+    while True:
+        if target >= float(coin_amount1[1]):
+            coin1 += 1
+        elif target >= float(coin_amount2[1]):
+            coin2 += 1
+        elif target >= float(coin_amount3[1]):
+            coin3 += 1
+        elif target >= float(coin_amount4[1]):
+            coin4 += 1
+        else:
+            break
+    print(f"To reach your goal you need \n{coin1} {coins[3[0]]} \n{coin2} {coins[2[0]]} \n{coin3} {coins[1[0]]} \n{coin4} {coins[0[0]]}")
+         
+
+
+
+
+def convert(country,target):
+    coin_amount1 = ""
+    coin_amount2 = ""
+    coin_amount3 = ""
+    coin_amount4 = ""
+    if country == "1":
+        country = 'America'
+        def denom_get(country,target,coins, coin_amount1, coin_amount2, coin_amount3, coin_amount4):
+            coins.clear
+            with open("Coin-change_problemo\money.csv", "r") as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    if row[2] == country:
+                        coins.append({"Name": row[0], "Worth": row[1]})
+                        if coin_amount1 == "":
+                            coin_amount1 = row[1]
+                        elif coin_amount2 == "":
+                            coin_amount2 = row[1]
+                        elif coin_amount3 == "":
+                            coin_amount3 = row[1]
+                        elif coin_amount4 == "":
+                            coin_amount4 = row[1]
+                    else:
+                          continue
+            return coins, coin_amount1, coin_amount2, coin_amount3, coin_amount4
+        denom_get(country, target,coins, coin_amount1, coin_amount2, coin_amount3, coin_amount4)
+        calculate(target, coin_amount1, coin_amount2, coin_amount3, coin_amount4)
+        
--- Coin-change_problemo/main.py
+++ Coin-change_problemo/main.py
@@ -1,5 +1,5 @@
 #Tate Morgan Coin Change Problem
-
+from converter import convert
 
 
 
@@ -14,9 +14,9 @@
 
 def main():
     print("WELCOME TO MY SOLUTION TO THE COIN CHANGE PROBLEM")
-
-
-
+    country = input("Press the number corresponding with the country are you translating from \n1. USA \n")
+    target = float(input("How much money would you like to save for please write it in 5.25 form \n"))
+    convert(country,target)
 
 
 main()
