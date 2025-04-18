6a72f95915363090e5adb9041148514d5b9c8692
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-04-18 21:37:50 +0000

    eee

--- Pet_simulator/main.py
+++ Pet_simulator/main.py
@@ -1,12 +1,32 @@
 #Main Menu for game
 from pet_management import *
+from time import sleep
+from threading import Timer
 
-
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.start()
     
-
-
-
-
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+    
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+    
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
 
 
 
@@ -28,8 +48,27 @@ def main():
         print(pet_sprites['pet_idle_o'])
     print(f'\n{current_koro.name} comes running over at your call')
     while True:
-        decrease_time = random.randint(30, 120)
-        choice = input('What would you like to do? (1: Feed, \n2: Play, \n3: Sleep, \n4: Check Status, \n5. Change pet, \n6. Quit) \n')
+        rt = RepeatedTimer(random.randint(30,120), stat_update(current_koro)) # it auto-starts, no need of rt.start()
+        try:
+            stat_update(current_koro)
+        finally:
+            rt.stop()
+            choice = input('What would you like to do? \n1: Feed, \n2: Play, \n3: Sleep, \n4: Check Status, \n5. Change pet, \n6. Quit \n')
+            if choice == 1:
+                pass
+            elif choice == 2:
+                pass
+            elif choice == 3:
+                pass
+            elif choice == 4:
+                current_koro.stat_check()
+            elif choice == 5:
+                print('You have chosen to change your pet')
+                current_koro = pet_selection()
+                print(current_koro)
+            elif choice == 6:
+                print('Thanks for playing!')
+                quit()
     
     
         
