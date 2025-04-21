f096b4c8f9abebc5e72698569bb981a7ebf7528d
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-04-21 15:01:24 +0000

    eee

--- Pet_simulator/main.py
+++ Pet_simulator/main.py
@@ -17,58 +17,50 @@ def main():
         current_koro = pet_selection()
         print(current_koro)
     stat_update(current_koro)
-    random_events.(current_koro)
+    random_events(current_koro)
     while True:
-        if Event_timer.is_alive() == True and Stat_timer.is_alive() == True:
-            if current_koro.shade == 'normal':
-                print(pet_sprites['pet_idle_normal'])
-            elif current_koro.shade == 'X faced':
-                print(pet_sprites['pet_idle_X'])
-            elif current_koro.shade == 'O faced':
-                print(pet_sprites['pet_idle_O'])
-            elif current_koro.shade == 'Rick':
-                print('Hello Mrs. Larose you have made a grave error choosing this pet, this will last around 3-5 seconds when it stops I recommend you choose another pet')
-                time.sleep(5)
-                rick()
-            elif current_koro.shade == 'I am not a pet, I am a god(striped)':
-                print(pet_sprites['pet_idle_stripe'])
-            print(f'\n{current_koro.name} comes running over at your call')
-            choice = int(input('What would you like to do? \n1: Feed, \n2: Play, \n3: Sleep, \n4: Check Status, \n5. Change pet, \n6. Fly(Requires level 2) \n7. Melt(Requires level 5) \n8. Quit \n'))
-            if choice == 1:
-                current_koro.feed()
-            elif choice == 2:
-                current_koro.play()
-            elif choice == 3:
-                current_koro.sleep()
-            elif choice == 4:
-                print(current_koro.stat_check())
-            elif choice == 5:
-                print('You have chosen to change your pet')
-                current_koro = pet_selection()
-                print(current_koro)
-            elif choice == 6:
-                if current_koro.level >= 2:
-                    fly(current_koro)
-                else:
-                    print('Your pet is not leveled up enough to fly')
-                    time.sleep(2)
-            elif choice == 7:
-                if current_koro.level >= 5:
-                    melt(current_koro)
-                else:
-                    print('Your pet is not leveled up enough to melt')
-                    time.sleep(2)
-            elif choice == 8:
-                print('Thanks for playing!')
-                save_pet(current_koro)
-                quit()
-        elif Event_timer.is_alive() == False and Stat_timer.is_alive() == False:
-            Event_timer = threading.timer(random.randint(60,180), random_events, args=(current_koro,)).start()
-            Stat_timer = threading.timer(random.randint(60,120), stat_update, args=(current_koro,)).start()
-        elif Event_timer.is_alive() == False:
-            Event_timer = threading.timer(random.randint(60,180), random_events, args=(current_koro,)).start()
-        elif Stat_timer.is_alive() == False:
-            Stat_timer = threading.timer(random.randint(60,120), stat_update, args=(current_koro,)).start()
+        if current_koro.shade == 'normal':
+            print(pet_sprites['pet_idle_normal'])
+        elif current_koro.shade == 'X faced':
+            print(pet_sprites['pet_idle_X'])
+        elif current_koro.shade == 'O faced':
+            print(pet_sprites['pet_idle_O'])
+        elif current_koro.shade == 'Rick':
+            print('Hello Mrs. Larose you have made a grave error choosing this pet, this will last around 3-5 seconds when it stops I recommend you choose another pet')
+            time.sleep(5)
+            rick()
+        elif current_koro.shade == 'I am not a pet, I am a god(striped)':
+            print(pet_sprites['pet_idle_stripe'])
+        print(f'\n{current_koro.name} comes running over at your call')
+        choice = int(input('What would you like to do? \n1: Feed, \n2: Play, \n3: Sleep, \n4: Check Status, \n5. Change pet, \n6. Fly(Requires level 2) \n7. Melt(Requires level 5) \n8. Quit \n'))
+        if choice == 1:
+            current_koro.feed()
+        elif choice == 2:
+            current_koro.play()
+        elif choice == 3:
+            current_koro.sleep()
+        elif choice == 4:
+            print(current_koro.stat_check())
+        elif choice == 5:
+            print('You have chosen to change your pet')
+            current_koro = pet_selection()
+            print(current_koro)
+        elif choice == 6:
+            if current_koro.level >= 2:
+                fly(current_koro)
+            else:
+                print('Your pet is not leveled up enough to fly')
+                time.sleep(2)
+        elif choice == 7:
+            if current_koro.level >= 5:
+                melt(current_koro)
+            else:
+                print('Your pet is not leveled up enough to melt')
+                time.sleep(2)
+        elif choice == 8:
+            print('Thanks for playing!')
+            save_pet(current_koro)
+            quit()
         while current_koro.time_alive >= 3:
             print('Your pet has leveled up!')
             current_koro.level += 1
